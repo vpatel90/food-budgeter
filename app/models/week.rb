f7d252ddd1a31f23cfbd445e26cc745624cf82de
Vivek Patel <vnp229@gmail.com>
2017-01-05 18:31:14 -0600

    compare to avg 10

--- app/models/week.rb
+++ app/models/week.rb
@@ -18,4 +18,20 @@ class Week < ApplicationRecord
     average.round(2)
   end
 
+  def saved_money?(hypothetical_spent)
+    hypothetical_spent - budget > 0 ? true : false
+  end
+
+  def savings_message(hypothetical_spent)
+    difference = hypothetical_spent - budget
+
+    if difference > 0
+      "You've saved $#{difference}"
+    elsif difference < 0
+      "You've overspent $#{difference.abs}"
+    else
+      "You've spent exactly the same"
+    end
+  end
+
 end
