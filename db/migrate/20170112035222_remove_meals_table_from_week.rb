48d9df8e21a820295eb1eddc176cd174561b46de
Vivek Patel <vnp229@gmail.com>
2017-01-11 23:01:12 -0600

    migration fix

--- db/migrate/20170112035222_remove_meals_table_from_week.rb
+++ db/migrate/20170112035222_remove_meals_table_from_week.rb
@@ -1,7 +1,9 @@
 class RemoveMealsTableFromWeek < ActiveRecord::Migration[5.0]
   def change
     Week.all.each do |week|
-      week.meals.create(manner: "eat_in")
+      week.meals.times do
+        Meal.create(manner: "eat_in", week_id: week.id)
+      end
     end
 
     remove_column :weeks, :meals
