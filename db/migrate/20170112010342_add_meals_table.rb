1d10286160b4dabfe83fd6a0200c24dd040f9003
Naghmeh Shirazi <naghmehd@gmail.com>
2017-01-11 22:33:27 -0600

    changed type to manner

--- db/migrate/20170112010342_add_meals_table.rb
+++ db/migrate/20170112010342_add_meals_table.rb
@@ -2,7 +2,7 @@ class AddMealsTable < ActiveRecord::Migration[5.0]
   def change
     create_table :meals do |t|
       t.belongs_to :week, foreign_key: true
-      t.string  :type
+      t.string  :manner
       t.boolean :free, default: false
 
       t.timestamps
