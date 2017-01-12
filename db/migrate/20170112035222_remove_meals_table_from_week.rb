1bb46c33a5254407bce7f8209f49da2ae4b8622b
Naghmeh Shirazi <naghmehd@gmail.com>
2017-01-11 22:40:10 -0600

    update schema

--- db/migrate/20170112035222_remove_meals_table_from_week.rb
+++ db/migrate/20170112035222_remove_meals_table_from_week.rb
@@ -1,7 +1,7 @@
 class RemoveMealsTableFromWeek < ActiveRecord::Migration[5.0]
   def change
     Week.all.each do |week|
-      week.meals.create(type: "eat_in")
+      week.meals.create(manner: "eat_in")
     end
 
     remove_column :weeks, :meals
