27477b747e048fc3ca6e8e536cd6c768920dc9de
Naghmeh <naghmehd@gmail.com>
2017-01-15 20:50:25 -0600

    added comparisons routes and controller (#44)
    * added comparisons routes and controller
    
    * added view for comparison index and deleted show from the routes for comparisons

---
+++ app/controllers/comparisons_controller.rb
@@ -0,0 +1,6 @@
+class ComparisonsController < ApplicationController
+
+  def index
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :weeks, only: :update
+  resources :weeks,        only: :update
   resources :expenditures, only: :update
-
+  resources :comparisons,  only: [:index]
 
 end
