70c920d2ab24bcfd4e55686b79dd0fa617cb4b43
COLIN <reckocp@gmail.com>
2017-01-17 19:11:53 -0600

    Route to the settings page

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
   resources :weeks,        only: :update
   resources :expenditures, only: :update
   resources :comparisons,  only: [:index]
+  get '/settings' => 'dashboard#settings'
+
+
 end
